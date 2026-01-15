2e811f9c63f4274cfc6dba41dfed2d35eaeb6eb2
Jaywardhan Charan <81804520+JaywardhanCharan@users.noreply.github.com>
2026-01-16 01:16:45 +0530

    Update App.js

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
+import NewContent from "./components/NewContent";
+import NewNavbar from "./components/NewNavbar";
+import Morecomponentstocome from "./components/Morecomponentstocome";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Content />
+      <NewContent />
+      <NewNavbar />
+      <Morecomponentstocome />
     </div>
       
   );
